ad098748a462ba3c9241fa2d1a96d95d6d98ca87
Sajadanjiidanii <154419813+Sajadanjiidanii@users.noreply.github.com>
2024-12-25 13:26:47 +0330

    Add files via upload

---
+++ Componets/cart-item/cart-item.js
@@ -0,0 +1,51 @@
+const $ = document
+let template = $.createElement("template")
+template.innerHTML = `
+    <link rel="stylesheet" href="../Icon/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../Componets/cart-item/cart-item.css">
+    <div class="item">
+        <div class="imgBox">
+            <img src="../Img/laptop item/laptop-item-1.png">
+        </div>
+        <div class="infoBox">
+            <h4 id="nameItem">لپتاپ لنوو</h4>
+            <div class="iconBox">
+                <i class="colorBox"></i>
+                <span id="item-color" class="textInfo">نقره ای</span>
+            </div>
+            <div class="iconBox">
+                <i class="fa fa-truck truck"></i>
+                <span class="textInfo">ارسال فوری</span>
+            </div>
+            <div class="iconBox">
+                <i class="fa fa-shield shield"></i>
+                <span class="textInfo">گارانتی 24 ماه</span>
+            </div>
+        </div>
+        <div class="priceBox">
+            <p id="oldPrice"></p>
+            <p id="newPrice"></p>
+        </div>
+        <div class="deletebox">
+            <i class="fa fa-trash-o"></i>
+        </div>
+    </div>
+`
+
+class cartItem extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({mode : "open"})
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+    connectedCallback(){
+        this.shadowRoot.querySelector("#nameItem").innerHTML = this.getAttribute("nameItem")
+        this.shadowRoot.querySelector("#oldPrice").innerHTML = this.getAttribute("oldPrice")
+        this.shadowRoot.querySelector("#newPrice").innerHTML = this.getAttribute("newPrice") + " تومان"
+        this.shadowRoot.querySelector("#item-color").innerHTML = this.getAttribute("ItemColor")
+        this.shadowRoot.querySelector(".colorBox").style.backgroundColor = this.getAttribute("colorCode")
+    }
+}
+
+
+export {cartItem}
